9c1d226b9699ada447d8709a49a4656adf79ac09
SunghaWoo <59195649+SunghaWoo@users.noreply.github.com>
2020-01-16 21:03:24 +0900

    Add files via upload

---
+++ 1st_week/practice5_Brightness/practice5_Brightness.ino
@@ -0,0 +1,20 @@
+#define LED 9
+void setup() {
+  //pinMode(LED,OUTPUT); 
+  Serial.begin(9600);
+  Serial.println("Start");
+}
+void loop() {
+  for(int i=0; i<=255; i++){
+    analogWrite(LED,i);
+    Serial.print("LED : ");
+    Serial.println(i);
+    delay(10);
+  }
+  /*for(int i=255; i>=0; i--){
+    analogWrite(LED,i);
+    Serial.print("LED : ");
+    Serial.println(i);
+    delay(10);
+  }*/
+}
